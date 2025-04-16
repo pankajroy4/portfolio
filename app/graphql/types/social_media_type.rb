7f5c58a774af73d17d23985bfb5b3604e4b7c586
pankajroy4 <pankajroy842002@gmail.com>
2025-04-16 17:59:16 +0530

    Social Media Icons Updated

--- app/graphql/types/social_media_type.rb
+++ app/graphql/types/social_media_type.rb
@@ -4,6 +4,6 @@ module Types
     field :name, String, null: false
     field :link, String, null: false
     field :font_aws_icon_class, String, null: false
-    field :bg_hash_code, String, null: false
+    field :bg_hash_code, String, null: true
   end
 end
