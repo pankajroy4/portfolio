95415b07e5460ea94e9c1efee753bca5a6340658
pankajroy4 <pankajroy842002@gmail.com>
2026-05-14 18:05:05 +0530

    endpoint updated in react

--- app/javascript/appolo/ApolloClient.js
+++ app/javascript/appolo/ApolloClient.js
@@ -2,7 +2,7 @@ import { ApolloClient, InMemoryCache, HttpLink } from "@apollo/client";
 
 const apClient = new ApolloClient({
   link: new HttpLink({
-    uri: "https://pankajroy.in/graphql",
+    uri: "https://pankajroy.online/graphql",
     // credentials: "include", // If using authentication, include credentials
   }),
   cache: new InMemoryCache(),
